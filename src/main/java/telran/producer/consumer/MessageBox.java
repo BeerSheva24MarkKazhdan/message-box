02fd13f2fbfc0374800dc2cee7b8d14090f105d7
Mark <kazhdan.mark@gmail.com>
2024-12-16 20:40:46 +0200

    CW49

---
+++ src/main/java/telran/producer/consumer/MessageBox.java
@@ -0,0 +1,7 @@
+package telran.producer.consumer;
+
+public interface MessageBox {
+    void put(String message)throws InterruptedException;
+    String take() throws InterruptedException;
+    String poll();
+}
